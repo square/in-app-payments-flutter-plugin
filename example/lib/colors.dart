710a6e4a68722471dda03587c8bae77e822e1dc7
Xiao Hu <xiao@squareup.com>
2018-12-13 10:33:00 -0800

    remove extra space from header comment end mark

--- example/lib/colors.dart
+++ example/lib/colors.dart
@@ -12,7 +12,7 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
- */
+*/
 import 'package:flutter/material.dart';
 
 Color monteCarlo = Color(0xFF78CCC5);
--- lib/in_app_payments.dart
+++ lib/in_app_payments.dart
@@ -12,7 +12,7 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
- */
+*/
 import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:built_value/standard_json_plugin.dart';
