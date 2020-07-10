b311d00d0c130ff3f4570cf6f92109ce5320c8bf
mohsin-sq <64042325+mohsin-sq@users.noreply.github.com>
2020-07-10 14:11:20 -0700

    Added documentation URL for Google Pay WalletConstants. (#122)

--- lib/google_pay_constants.dart
+++ lib/google_pay_constants.dart
@@ -13,6 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+//https://developers.google.com/android/reference/com/google/android/gms/wallet/WalletConstants
+
 const int totalPriceStatusNotCurrentlyKnown = 1;
 const int totalPriceStatusEstimated = 2;
 const int totalPriceStatusFinal = 3;
